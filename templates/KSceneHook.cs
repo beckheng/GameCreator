1c5cdb7211948f858dabb9db100125874d22ba90
Beckheng Lam <beckheng@gmail.com>
2017-12-07 22:49:20 +0800

    添加模板文件 KSceneHook

---
+++ templates/KSceneHook.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace KScene
+{
+
+	public class KSceneHook  {
+
+		public static System.Type GetManagerType(string sceneName)
+		{
+			//if (sceneName.StartsWith("XXXX_"))
+			//{
+			//	return typeof(XXXSceneManager); // of cause create XXXSceneManager first
+			//}
+
+			return null;
+		}
+
+	}
+
+}
